00066867f3bb654116fe9e34a94fc28e2b329e79
萌形象1234 <7567005+cute_image_1234@user.noreply.gitee.com>
2022-08-01 20:41:41 +0800

    home 轮播图完成

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
@@ -8,18 +7,53 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
+// 按需导入 $http 对象
+import {
+	$http
+} from '@escook/request-miniprogram'
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+// 配置请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	// do somethimg...
+	//展示 loading 效果：
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+// 响应拦截器
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+	// do something...
+	//隐藏 loading 效果：
+	uni.hideLoading()
+}
+
+// 封装弹窗的方法
+uni.$showMsg = function(title = "请求失败",duration = 2000){
+	uni.showToast({
+		title,
+		duration,
+		icon: "none"
+	})
+}
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,14 @@
 <template>
 	<view>
-		home
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="item in swiperList" :key="item.goods_id">
+				<view class="swiper-item">
+					<navigator :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+						<image :src="item.image_src"></image>
+					</navigator>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +16,34 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图的数据
+				swiperList:[]
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+			
+		},
+		// 轮播图数据
+		methods:{
+			async getSwiperList(){
+				const {data:res} = await uni.$http.get("/home/swiperdata")
+				if(res.meta.status !==200) return uni.$showMsg()
+			    this.swiperList = res.message
+			}
 		}
 	}
+	
 </script>
 
 <style lang="scss">
-
+swiper{
+	height: 330rpx;
+	.swiper-item,
+	navigator,
+	image{
+		width: 100%;
+		height: 100%;
+	}
+}
 </style>
